8daf0888fe72352ae855b6558444c22d2c51e854
lidawn <lidawn1991@gmail.com>
2015-09-05 22:13:47 +0800

    qlist

--- Ccode/list.c
+++ Ccode/list.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define list_entry(ptr,type,member) (type*)((char*)ptr - (unsigned int)(&((type*)0->member)))
+//根据ptr 定位结构体 
+#define LIST_ENTRY(ptr,type,member) (type*)((char*)ptr - (unsigned int)( &( ((type*)0)->member ) ))
 
-#define for_list_each(ptr,type) for(ptr=type->member;ptr!=ptr->next;ptr=ptr->next)
+//遍历双链表 
+#define FOR_LIST_EACH(pos,head) for(pos=(head)->next;pos!=(head);pos=pos->next)
 
+//这两种方法为什么要有？ 
 #define LIST_HEAD_INIT(name) {&(name),&(name)}
 
 #define LIST_HEAD(name) struct qlist name = LIST_HEAD_INIT(name)
@@ -24,32 +27,45 @@ struct my_list{
 	int value;
 };
 
-static void qlist_add(struct qlist *new,struct qlist *head){
-	struct qlist *tmp = head->next;
-	head->next = new;
-	head->next->pre = new;
-	new->next = tmp;
-	new->pre = head;
+//将new_p插到pre和next之间 
+static void __qlist_add(struct qlist *new_p,struct qlist *pre,struct qlist *next){
+	next->pre = new_p;
+	new_p->next = next;
+	new_p->pre = pre;
+	pre->next = new_p;
+}
+
+//表头插入 
+static void qlist_add(struct qlist *new_p,struct qlist *head){
+	__qlist_add(new_p,head,head->next);
+}
+
+//表尾插入 
+static void qlist_add_tail(struct qlist *new_p,struct qlist *head){
+	__qlist_add(new_p,head->pre,head); 
 }
 
 
+
 int main(){
-	struct my_list *test;
-	struct my_list *tmp; 
+	struct my_list *tmp,*tail; 
+	struct qlist *head_link,*link;
 	int i=0;
-	test = (struct my_list*)malloc(sizeof(struct my_list));
-	test->value = 1;
 	
-	INIT_LIST_HEAD(&(test->head));
+	head_link = (struct qlist*)malloc(sizeof(struct qlist));
+	INIT_LIST_HEAD(head_link);
 	
-	/*
 	for(i=0;i<5;i++){
 		tmp = (struct my_list*)malloc(sizeof(struct my_list));
-		tmp->value = i+2;
-		list_init(tmp->head);
-		qlist_add(tmp->head,test->head);
+		tmp->value = i;
+		INIT_LIST_HEAD(&(tmp->head));
+		qlist_add(&(tmp->head),head_link);
 	}
-	*/
+	
+	FOR_LIST_EACH(link,head_link){
+		tail = LIST_ENTRY(link,struct my_list,head);
+		printf("%d\n",tail->value);
+	}
 	
 	return 0;
 }
